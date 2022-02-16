83495258385e6101dae1223ea2439cf6d3122791
cavem <kyle.stinar@hotmail.com>
2022-02-16 11:54:24 -0800

    second

---
+++ index.js
@@ -0,0 +1,14 @@
+const traffic = require("./passwords");
+
+// const userAction = async (traffic) => {
+//   console.log("fire");
+//   const response = await fetch(
+//     "http://wsdot.wa.gov/Traffic/api/TrafficFlow/TrafficFlowREST.svc/GetTrafficFlowsAsJson?AccessCode=6907939f-9e50-4878-b708-6257f9d83ee3"
+//   );
+//   const myJson = await response.json(); //extract JSON from the http response
+//   // do something with myJson
+//   return myJson;
+// };
+
+// module.exports = userAction;
+// console.log(userAction());
